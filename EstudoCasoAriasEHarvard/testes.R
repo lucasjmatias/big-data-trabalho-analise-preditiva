74cb485045978873760791b16a9589c5f244cb86
Lucas Matias <lucas.matias.87@gmail.com>
2018-11-21 22:25:00 -0200

    ajustes

--- EstudoCasoAriasEHarvard/testes.R
+++ EstudoCasoAriasEHarvard/testes.R
@@ -10,6 +10,17 @@ pagamentoEmprestimo$sexo <- as.numeric(pagamentoEmprestimo$sexo)
 
 glm(data = pagamentoEmprestimo, formula = pagamento ~ estadocivil + idade + sexo, family = binomial) -> glmPagamento
 summary(glmPagamento)
+coef(glmPagamento)
+glmprobs <- predict(glmPagamento, type="response")
 
+nLinhas <- nrow(pagamentoEmprestimo)
+glmpred <- rep(0, nLinhas)
+glmpred[ glmprobs > 0.5 ] <- 1
+View(glmpred)
 
+View(pagamentoEmprestimo$pagamento)
+
+table(glmpred, pagamentoEmprestimo$pagamento)
+
+glmprobs
 ggpairs(data = pagamentoEmprestimo, ggplot2::aes(color=as.factor(pagamento)))
