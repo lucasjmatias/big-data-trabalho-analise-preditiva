da1eec075b86331eb38a120a709d9a9ff91aa206
Lucas Matias <lucas.matias.87@gmail.com>
2018-11-24 15:28:03 -0200

    evolução análise costumer churn

--- EstudoCasoAriasEHarvard/testes_costumer_churn.R
+++ EstudoCasoAriasEHarvard/testes_costumer_churn.R
@@ -1,46 +1,56 @@
+#install.packages("ROCR")
+#install.packages("caret")
+#install.packages("ResourceSelection")
+#install.packages("pROC")
+
+#library(pROC)
+#library(ResourceSelection)
+#library(ROSE)
+#library(ROCR)
+#library(MASS)
+#library(caret)
+
 customerChurn <-
   read.csv2("./dados/estudo_caso_customer_churn.csv", stringsAsFactors = FALSE)
 
 
-ggpairs(data = customerChurn, ggplot2::aes(color=as.factor(churn)))
-
-table(customerChurn$costumer_age)
-
-customerChurn$customer_age
 #Preparar dados
 customerChurn$clientes_novos <- 0
 customerChurn$clientes_novos[customerChurn$customer_age < 6] <- 1
 customerChurn$clientes_risco <- 0
-customerChurn$clientes_risco[customerChurn$customer_age > 6 & customerChurn$customer_age <= 14] <- 1
-
-
-str(customerChurn)
-View(customerChurn)
-#O que é um chi-score baixo?
-boxplot(customerChurn$chi_score_month_1)
-boxplot(customerChurn$chi_score_month_0)
-
-summary(customerChurn$chi_score)
-summary(customerChurn$chi_score_month_0)
-summary(customerChurn$chi_score[customerChurn$churn == 1])
-
-str(customerChurn)
-
+customerChurn$clientes_risco[customerChurn$customer_age >= 6 &
+                             customerChurn$customer_age <= 17] <- 1
 
 
 glm(data = customerChurn,
-    formula = churn ~ chi_score_month_0   +
-      views_0_1 + days_since_last_login_0_1 +
-      clientes_novos + clientes_risco, family = binomial) -> 
+    formula = churn ~ clientes_risco + support_cases_month_0 +  support_cases_0_1 + 
+      days_since_last_login_0_1 + chi_score_month_0  , family = binomial) -> 
         glmCustomer
-
 summary(glmCustomer)
+
+
 glmprobsCostumer <- predict(glmCustomer, type="response")
+
 nLinhasCostumer <- nrow(customerChurn)
 glmpredCostumer <- rep(0, nLinhasCostumer)
-glmpredCostumer[ glmprobsCostumer > 0.05 ] <- 1
-table(glmpredCostumer, customerChurn$churn) -> tabelaCostumerChurn
-tabelaCostumerChurn
-(as.vector(tabelaCostumerChurn)[1] + as.vector(tabelaCostumerChurn)[4]) / nLinhasCostumer
 
-table(customerChurn$churn)
+rocobj <- roc(customerChurn$churn, glmprobsCostumer)
+coords(rocobj, x="best", input="threshold", best.method="youden")[1] -> pontoCorte
+pontoCorte
+
+glmpredCostumer[ glmprobsCostumer > pontoCorte ] <- 1
+dadosReais <- customerChurn$churn
+table(glmpredCostumer, dadosReais) -> tabelaCostumerChurn
+tabelaCostumerChurn
+
+(as.vector(tabelaCostumerChurn)[1] + as.vector(tabelaCostumerChurn)[4]) / nLinhasCostumer
+mean(customerChurn$churn == glmpredCostumer)
+
+sensitivity(tabelaCostumerChurn)
+specificity(tabelaCostumerChurn)
+
+
+
+
+
+
